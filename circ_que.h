981e6ce5f9a27b51d80d9ddd977e1401040a5613
Walt Karas <wkaras@yahoo.com>
2016-08-13 20:55:42 -0400

    Add comments to circ_que.h.

--- circ_que.h
+++ circ_que.h
@@ -17,6 +17,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+// Circular queue containers, usable in single-thread and multithread
+// code.
+
 // Include once.
 #ifndef ABSTRACT_CONTAINER_CIRC_QUE_H_
 #define ABSTRACT_CONTAINER_CIRC_QUE_H_
@@ -47,14 +50,46 @@ Circular queue container class template
 The 'abstractor' template parameter class must have the following public
 or protected members, or behave as though it does:
 
-type elem_t -- 
-type front_t -- 
-type back_t -- 
-static const man_num_elems
+Types:
+elem_t -- type of the elements of the circular queue.
 
-bool wait_while_empty()
-bool wait_while_full()
-bool
+Constants:
+unsigned max_num_elems -- Maximum number of elements that can be queued at the
+  same time.
+
+Member Functions:
+void consume_front(unsigned) -- change the index (initialy zero) of the front
+  element in the queue's underlying array.  This is only called from the
+  popping/consuming code.
+void produce_next_in(unsigned) -- change the index (initialy zero) of the next
+  element to push in the queue's underlying array.  This is only called from
+  the pushing/producing code.
+unsigned consume_front() -- returns the index of the front element in the
+  queue's underlying array.  This is only called from the popping/consuming
+  code.
+unsigned consume_next_in() -- returns the index of the next element in the
+  queue's underlying array to push.  This is only called from the
+  popping/consuming code.
+unsigned produce_front() -- returns the index of the front element in the
+  queue's underlying array.  This is only called from the pushing/producing
+  code.
+unsigned produce_next_in() -- returns the index of the next element in the
+  queue's underlying array to push.  This is only called from the
+  pushing/producing code.
+void end_full_wait() -- in blocking, mutithreading specializations, will cause
+  wait_while_full() call in the pushing thread to exit.  Should only be called
+  in popping thread.  Should return immediately in non-blocking
+  specializations.
+void end_empty_wait() -- in blocking, mutithreading specializations, will cause
+  wait_while_empty() call in the popping thread to exit.  Should only be called
+  in pushing thread.  Should return immediately in non-blocking
+  specializations.
+void wait_while_empty() -- block in popping thread, waiting for
+  end_empty_wait() to be called in the pushing thread.  Can be omitted in
+  non-blocking specializations.
+void wait_while_full() -- block in pushing thread, waiting for
+  end_full_wait() to be called in the popping thread.  Can be omitted in
+  non-blocking specializations.
 */
 template <class abstractor>
 class circ_que : protected abstractor
@@ -80,6 +115,8 @@ class circ_que : protected abstractor
     elem_t & operator [] (unsigned idx)
       { return(reinterpret_cast<elem_t &>(data[idx])); }
 
+    // Returns number of elements currently in queue, given index of front
+    // element, and index of next element to push.
     static unsigned size(unsigned f, unsigned ni)
       { return(ni >= f ? (ni - f) : (ni + dimension - f)); }
 
@@ -96,6 +133,9 @@ class circ_que : protected abstractor
 
   }; // end circ_que
 
+// Access queue from popping/consuming code.  The parameter is presumed
+// to be a specialization of circ_que.
+//
 template <class cq_t>
 class circ_que_front
   {
@@ -105,6 +145,7 @@ class circ_que_front
 
     circ_que_front(cq_t &cq_) : cq(cq_) { }
 
+    // Returns number of elements currently in queue.
     unsigned size() const
       {
         unsigned ni = cq.consume_next_in();
@@ -124,14 +165,19 @@ class circ_que_front
           cq.wait_while_empty();
       }
 
+    // Returns element currently at front of queue.
     const elem_t & operator () () const
       { return(cq[cq.consume_front()]); }
 
+    // Look ahead from front to successive elements in queue.  index
+    // must be less than size().
     const elem_t & operator () (unsigned index) const
       { return(cq[(cq.consume_front() + index) % cq_t::dimension]); }
 
+    // Pop (discard) front element, without calling ~elem_t().
     void pop(raw_t) { pop_(cq.consume_front()); }
 
+    // Pop (discard) front element.
     void pop()
       {
         unsigned f = cq.consume_front();
@@ -156,6 +202,9 @@ class circ_que_front
 
   }; // end circ_que_front
 
+// Access queue from pushing/producing code.  The parameter is presumed
+// to be a specialization of circ_que.
+// 
 template <class cq_t>
 class circ_que_back
   {
@@ -165,6 +214,7 @@ class circ_que_back
 
     circ_que_back(cq_t &cq_) : cq(cq_) { }
 
+    // Returns number of elements currently in queue.
     unsigned size() const
       {
         unsigned ni = cq.produce_next_in();
@@ -180,10 +230,14 @@ class circ_que_back
           cq.wait_while_full();
       }
 
+    // Return reference to next element to push (which has not been
+    // initialized).
     elem_t & operator () () { return(cq[cq.produce_next_in()]); }
 
+    // Push next element, without initializing it.
     void push(raw_t) { push_(cq.produce_next_in()); }
 
+     // Construct the next element to push and then push it.
     template<typename ... args_t>
     void push(args_t && ... args)
       {
@@ -209,6 +263,10 @@ class circ_que_back
 
   }; // end circ_que_back
 
+// Abstractor for single-thread instantiation of circ_que. index_t
+// is an unsigned integral type large enough to hold values up to
+// max_num_elems;
+//
 template <
   typename elem_t_, unsigned max_num_elems_, typename index_t = unsigned>
 class circ_que_abs_basic
@@ -217,7 +275,7 @@ class circ_que_abs_basic
 
     typedef elem_t_ elem_t;
 
-    static const index_t max_num_elems = max_num_elems_;
+    static const unsigned max_num_elems = max_num_elems_;
 
     index_t front, next_in;
 
@@ -237,6 +295,7 @@ class circ_que_abs_basic
     void end_empty_wait() { }
   };
 
+// Specialization for single-thread use.
 template <typename elem_t, unsigned max_num_elems, typename index_t = unsigned>
 using basic_circ_que =
   circ_que<circ_que_abs_basic<elem_t, max_num_elems, index_t> >;
